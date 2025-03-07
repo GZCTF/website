b870a3bfecf6668e6b60619334ef0dc43c318dd5
GZTime <Time.GZ@outlook.com>
2025-03-07 23:10:36 +0800

    chore: upgrade to rspress v2

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,5 +1,5 @@
 import { LogoHeader } from "@Components/LogoHeader";
-import Theme from "rspress/theme";
+import { Layout as BasicLayout } from 'rspress/theme';
 import ReactGA from "react-ga4";
 import { clarity } from "react-microsoft-clarity";
 
@@ -8,14 +8,8 @@ const setup = () => {
   clarity.init("oossz1i1h3");
 };
 
-const Layout = () => {
-  return <Theme.Layout navTitle={<LogoHeader />} />;
-};
+const Layout = () => <BasicLayout navTitle={<LogoHeader />} />;
 
-export default {
-  ...Theme,
-  Layout,
-  setup,
-};
+export { Layout, setup };
 
 export * from "rspress/theme";
